2531828ac055fef6e81bbbee22fcad4aa6135005
Carsio <carsio1256@gmail.com>
2017-09-07 20:22:08 -0400

    Funcionalidades estao responsivas | Barra de navegacao responsiva

--- public_html/header-standart.php
+++ public_html/header-standart.php
@@ -3,6 +3,7 @@
 <head>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<title>Agenda</title>
 	<link href=<?php echo "https://".$_SERVER['SERVER_NAME']."/img/agenda-icone.ico"; ?> rel="shortcut icon"/>
 	<meta charset="utf-8">
@@ -15,37 +16,41 @@
 </head>
 <body>
 
-<div class="container">
+	<div class="container">
 
-	<nav class="navbar navbar-inverse">
+		<nav class="navbar navbar-inverse">
 
-	  	<div class="container-fluid">
-	    	<div class="navbar-header">
-	     		<a class="navbar-brand" href="https://carsio.000webhostapp.com/contato">
-	       			Agenda Virtual
-	      		</a>
-	    	</div>
+			<div class="container-fluid">
 
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href=<?php echo "https://".$_SERVER['SERVER_NAME']."/contato"; ?>>Agenda Virtual</a>
+				</div>
 
-	    	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	    		<ul class="nav navbar-nav navbar navbar-right">
-	    			<li>
-	    				<div class="btn-group navbar-btn" role="group" aria-label="...">
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar navbar-right">
+						<li>
+							<div class="btn-group navbar-btn" role="group" aria-label="...">
 
-	    					<button class="btn btn-default" onclick="location.href='<?php echo "https://".$_SERVER['SERVER_NAME']."/entrar"; ?>'">
-	    						<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-	    						Entrar
-	    					</button>
+								<button class="btn btn-default" onclick="location.href='<?php echo "https://".$_SERVER['SERVER_NAME']."/entrar"; ?>'">
+									<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+									Entrar
+								</button>
 
-	    					<button class="btn btn-default" onclick="location.href='<?php echo "https://".$_SERVER['SERVER_NAME']."/registrar"; ?>'">
-	    						<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-	    						Registrar
-	    					</button>
-	    					
-	    				</div>
-	    			</li>
-	    			</ul>
-		    </div>
-	 	</div>   
-	</nav>
-</div>
+								<button class="btn btn-default" onclick="location.href='<?php echo "https://".$_SERVER['SERVER_NAME']."/registrar"; ?>'">
+									<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+									Registrar
+								</button>
+
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>   
+		</nav>
+	</div>
--- public_html/header.php
+++ public_html/header.php
@@ -3,6 +3,7 @@
 <head>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<title>Agenda</title>
 	<link href=<?php echo "https://".$_SERVER['SERVER_NAME']."/img/agenda-icone.ico"; ?> rel="shortcut icon"/>
 	<meta charset="utf-8">
@@ -15,46 +16,53 @@
 </head>
 <body>
 
-<div class="container">
+	<div class="container">
 
-	<nav class="navbar navbar-inverse">
-
-	  	<div class="container-fluid">
-	    	<div class="navbar-header">
-	     		<a class="navbar-brand" href=<?php echo "https://".$_SERVER['SERVER_NAME']."/contato"; ?>>
-	       			Agenda Virtual
-	      		</a>
-	    	</div>
+		<nav class="navbar navbar-inverse">
 
 
-	    	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	    		<ul class="nav navbar-nav navbar navbar-right">
-	    			<li>
-	    				<div class="btn-group navbar-btn" role="group" aria-label="...">
-	    					<button class="btn btn-default" onclick="location.href='/novo'">
-	    						<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-	    						Novo Contato
-	    					</button>
-	    					<button class="btn btn-default" onclick="location.href='/contato';" id="myButton">
-	    						<span class="glyphicon glyphicon-book" aria-hidden="true"></span>
-	    						Contatos
-	    					</button>
-	    					<button class="btn btn-default">
-	    						<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-	    						<?php echo $_SESSION['nome_user']; ?>
-	    					</button>
-	    					<button class="btn btn-default">
-	    						<span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>
-	    						Sobre
-	    					</button>
-	    					<button class="btn btn-default" onclick="location.href='/sair'">
-	    						<span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-	    						Sair
-	    					</button>
-	    				</div>
-	    			</li>
-	    			</ul>
-		    </div>
-	 	</div>   
-	</nav>
-</div>
+			
+
+			<div class="container-fluid">
+
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href=<?php echo "https://".$_SERVER['SERVER_NAME']."/contato"; ?>>Agenda Virtual</a>
+				</div>
+
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar navbar-right">
+						<li>
+							<div class="btn-group navbar-btn" role="group" aria-label="...">
+								<button class="btn btn-default" onclick="location.href='/novo'">
+									<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+									Novo Contato
+								</button>
+								<button class="btn btn-default" onclick="location.href='/contato';" id="myButton">
+									<span class="glyphicon glyphicon-book" aria-hidden="true"></span>
+									Contatos
+								</button>
+								<button class="btn btn-default">
+									<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+									<?php echo $_SESSION['nome_user']; ?>
+								</button>
+								<button class="btn btn-default">
+									<span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>
+									Sobre
+								</button>
+								<button class="btn btn-default" onclick="location.href='/sair'">
+									<span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+									Sair
+								</button>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>   
+		</nav>
+	</div>
